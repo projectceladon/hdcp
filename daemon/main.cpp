407d3db63d082750b9c945a524431c577d9a87a2
Yu Shiqiang <shiqiang.yu@intel.com>
2018-11-06 20:13:00 -0500

    Merge pull request #7
    Add Android makefile for hdcp

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -49,7 +49,7 @@ FILE *dmLog = nullptr;
 
 bool AlreadyRunning()
 {
-    const std::string pidFile = "/var/run/hdcpd.pid";
+    const std::string pidFile = HDCP_PIDFILE;
 
     int fd = open(pidFile.c_str(),
                 O_RDWR | O_CREAT,
@@ -57,7 +57,6 @@ bool AlreadyRunning()
     if (fd < 0)
     {
         HDCP_ASSERTMESSAGE("Could not open pid file : %s\n", pidFile.c_str());
-        close(fd);
         return true;
     }
 
@@ -97,7 +96,7 @@ int32_t daemon_init(void)
     {
         exit(SUCCESS);    // parent exit
     }
-    
+
     setsid();   // become session leader
 
     close(0);   // close stdin
