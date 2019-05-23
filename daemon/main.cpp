536716cb4eecee3687d63ebc61bb4c57816582e9
Badiuzzaman Iskhandar <badiuzzaman.azzarfan.bin.iskhandar@intel.com>
2019-05-23 17:19:48 +0800

    Enable logging for Android, remove redundant \n
    Signed-off-by: Badiuzzaman Iskhandar <badiuzzaman.azzarfan.bin.iskhandar@intel.com>

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -57,7 +57,7 @@ bool AlreadyRunning()
                 S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (fd < 0)
     {
-        HDCP_ASSERTMESSAGE("Could not open pid file : %s\n", pidFile.c_str());
+        HDCP_ASSERTMESSAGE("Could not open pid file : %s", pidFile.c_str());
         return true;
     }
 
@@ -69,7 +69,7 @@ bool AlreadyRunning()
 
     if (fcntl(fd, F_SETLK, &fl) < 0)
     {
-        HDCP_ASSERTMESSAGE("Could not lock pid file\n");
+        HDCP_ASSERTMESSAGE("Could not lock pid file");
         close(fd);
         return true;
     }
@@ -77,7 +77,7 @@ bool AlreadyRunning()
     std::string pid = std::to_string(getpid());
     if (write(fd, pid.c_str(), pid.length()) < 0)
     {
-        HDCP_ASSERTMESSAGE("Could not write pid file\n");
+        HDCP_ASSERTMESSAGE("Could not write pid file");
         close(fd);
         return true;
     }
@@ -218,7 +218,7 @@ int32_t main(void)
 
     if (AlreadyRunning())
     {
-        HDCP_ASSERTMESSAGE("hdcp aleady already running\n");
+        HDCP_ASSERTMESSAGE("hdcp aleady already running");
         return 1;
     }
 
