be4133047bee972f915818edec6b05c788328eaf
VenkataSainath Ravikanti <venkatasainath.ravikanti@intel.com>
2019-08-20 11:11:49 +0800

    MUST_REBASE [IOTG]: compile: Compilation error fixes.
    This patch fixes compilation issues after 19.2.0 rebase
    
    Change-Id: I14912137dccdc6f05baba2599a7b8be0f1931f95
    Tracked-On:
    Signed-off-by: VenkataSainath Ravikanti <venkatasainath.ravikanti@intel.com>

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -242,11 +242,12 @@ int32_t main(void)
     {
         return 1;
     }
-
+#ifndef ANDROID
     if(ias_env)
     {
         util_create_display(0);
     }
+#endif
 
 #ifdef HDCP_LOG_FILE
     if (nullptr == dmLog)
@@ -277,10 +278,12 @@ int32_t main(void)
         fclose(dmLog);
         dmLog = nullptr;
     }
+#ifndef ANDROID
     if(ias_env)
     {
         util_destroy_display(0);
     }
+#endif
 
     HDCP_FUNCTION_EXIT(ret);
     return ret;
